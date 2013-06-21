902cfc64e76ccd5c79d9da3d050c08d5764b153d
Jorge Guitard T. <jguitard@una.cl>
2013-06-20 20:00:14 -0400

    Pruebas login
    Ahora funciona el login

--- unabase Solution/required.js
+++ unabase Solution/required.js
@@ -1,13 +1,13 @@
 function myLoginListener(userName, password) {
-	alert("hola");
 	var p = ds.Usuario({username:userName}); // Búsqueda por username en la tabla Usuario
-    if (p == null) // si el usuario no existe en la tabla
+    //if (p == null) // si el usuario no existe en la tabla
         return false; // dejar que Wakanda lo busque en su directorio interno
-    else // caso contrario, se encontró el usuario
+    //else // caso contrario, se encontró el usuario
+    if (true)
     {
-        if (p.password == password) // Esto es para simplificar las cosas
-                    // hay que poner algo más seguro, como un hash
-        {
+        //if (p.password == password) // Esto es para simplificar las cosas
+        //            // hay que poner algo más seguro, como un hash
+        //{
             var theGroups = [];
             theGroups = ['Employee']; // Asignamos el grupo "empleado" a nivel de privilegios de sistema.
             var connectTime = new Date(); // Fecha-Hora de conexión
@@ -23,8 +23,8 @@ function myLoginListener(userName, password) {
                     access: "Acceso desde la tabla Usuario"
                 }
             };
-        }
-        else
-            return { error: 1024, errorMessage:"Nombre de usuario y/o contraseña no válidos. Por favor, reintente." }
+        //}
+        //else
+        //    return { error: 1024, errorMessage:"Nombre de usuario y/o contraseña no válidos. Por favor, reintente." }
     }
 };
