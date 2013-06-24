336c5697d135ea655c2f68a81be3879ba00e8cf0
Jorge Guitard T. <jguitard@una.cl>
2013-06-24 17:20:25 -0400

    Grilla Proyectos

--- unabase/Model.js
+++ unabase/Model.js
@@ -1,14 +1,4 @@
 ﻿
 guidedModel =// @startlock
 {
-	DetalleCotizacion :
-	{
-		totalNeto :
-		{
-			onGet:function()
-			{// @endlock
-				return 0.00;
-			}// @startlock
-		}
-	}
 };// @endlock
---
+++ unabase/import.js
@@ -0,0 +1,24 @@
+﻿function importData(entityName, dataSource, idFieldName, dataFieldStruct) {
+	console.info("Count items-before: " + dataSource.length);
+
+	var lines = loadText( ds.getModelFolder().path + "Import/" + entityName + ".txt" ).split("\n");
+	var columns = [];
+
+	lines.forEach(function(line) {
+		columns = line.split("\t");    
+
+		var item = dataSource.find(idFieldName + " = :1", columns[0]);
+		if(item == null) {
+			item = new dataSource({
+				ID: columns[0],
+				referencia: columns[1]
+			});
+			item.save();
+		}
+
+	});
+
+	console.info("Count items-after: " + dataSource.length);
+}
+
+importData("Proyecto", ds.Proyecto, "ID", null);
