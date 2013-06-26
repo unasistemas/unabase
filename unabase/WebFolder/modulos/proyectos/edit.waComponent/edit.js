6db7610548ece5b036741a719d7166af6dd680be
Jorge Guitard T. <jguitard@una.cl>
2013-06-26 17:25:34 -0400

    Data
    Data agregada a GitHub (para poder tener datos de prueba disponibles a
    todos).

--- unabase/WebFolder/modulos/proyectos/edit.waComponent/edit.js
+++ unabase/WebFolder/modulos/proyectos/edit.waComponent/edit.js
@@ -15,11 +15,18 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var textFieldNombreProyecto = {};	// @textField
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	textFieldNombreProyecto.change = function textFieldNombreProyecto_change (event)// @startlock
+	{// @endlock
+		sources.proyecto.save();
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_textFieldNombreProyecto", "change", textFieldNombreProyecto.change, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
