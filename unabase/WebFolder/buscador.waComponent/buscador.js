dd5c67015c42e83f78ebd81df8934b67d1f308e0
Jorge Guitard T. <jguitard@una.cl>
2013-06-26 10:39:59 -0400

    Buscador y maqueta de barra lateral

--- unabase/WebFolder/buscador.waComponent/buscador.js
+++ unabase/WebFolder/buscador.waComponent/buscador.js
@@ -7,7 +7,7 @@ function constructor (id) {
 
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
-	this.name = 'proyectos';
+	this.name = 'buscador';
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
