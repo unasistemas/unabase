3d8255d4a8ac4100aed90f3a5663715cf0119c99
Jorge Guitard T. <jguitard@una.cl>
2013-06-28 11:47:34 -0400

    Modelo de datos
    Nuevas modificaciones

--- unabase2/Model.js
+++ unabase2/Model.js
@@ -1,4 +1,21 @@
 ﻿
 guidedModel =// @startlock
 {
+	Contacto :
+	{
+		docIdentidad :
+		{
+			events :
+			{
+				onValidate:function(attributeName)
+				{// @endlock
+					switch(attributeName) {
+						case "docIdentidad":
+							this[attributeName].lpad("\0", 16);
+						break;
+					}
+				}// @startlock
+			}
+		}
+	}
 };// @endlock
