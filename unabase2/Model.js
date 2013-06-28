09576aca2156c795ba507a3aaa715991ae2aed47
Jorge Guitard T. <jguitard@una.cl>
2013-06-28 18:13:28 -0400

    Formulario de prueba
    Formulario para Sistema

--- unabase2/Model.js
+++ unabase2/Model.js
@@ -1,6 +1,76 @@
 ﻿
 guidedModel =// @startlock
 {
+	Usuario :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isUsuario = true;
+			}// @startlock
+		}
+	},
+	Sistema :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isSistema = true;
+			}// @startlock
+		}
+	},
+	Empresa :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isEmpresa = true;
+			}// @startlock
+		}
+	},
+	EntradaEmpleado :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isEmpleado = true;
+			}// @startlock
+		}
+	},
+	Persona :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isPersona = true;
+			}// @startlock
+		}
+	},
+	EntradaCliente :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isCliente = true;
+			}// @startlock
+		}
+	},
+	EntradaProveedor :
+	{
+		events :
+		{
+			onValidate:function()
+			{// @endlock
+				this.isProveedor = true;
+			}// @startlock
+		}
+	},
 	Contacto :
 	{
 		docIdentidad :
